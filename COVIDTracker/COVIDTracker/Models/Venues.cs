36ea8ee80fa7e14b1d9cbca267e00c156527a6a7
Alex Griffin <alexgriffin.dev@gmail.com>
2020-12-03 12:01:55 +0000

    Added Models
    Added user and venue models

---
+++ COVIDTracker/COVIDTracker/Models/Venues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVIDTracker.Models
+{
+    public class Venues
+    {
+        public int VenueId { get; set; }
+        public string VenueName { get; set; }
+        public int Capacity { get; set; }
+        public int BuildingNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public List<Users> UsersList { get; set; }
+    }
+}
